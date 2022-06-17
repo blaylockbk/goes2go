8c4ca5f1000dd17718e333a823125d4ff236fa6b
Brian Blaylock <blaylockbk@gmail.com>
2022-06-17 19:54:04 +0000

    updates

--- docs/conf.py
+++ docs/conf.py
@@ -89,7 +89,7 @@ html_theme = "pydata_sphinx_theme"
 html_theme_options = {
     "github_url": "https://github.com/blaylockbk/goes2go",
     "twitter_url": "https://twitter.com/blaylockbk",
-    "navbar_end": ["navbar-icon-links.html", "search-field.html"],
+    "navbar_end": ["theme-switcher", "navbar-icon-links.html", "search-field.html"],
     "google_analytics_id": "G-NWK8MJNNGX",
     "use_edit_page_button": True,
     "show_toc_level": 1,
