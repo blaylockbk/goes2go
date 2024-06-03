47a6bd8e986af3d157331b9ba4cf978043aa5323
ibarlet <104371002+ibarlet@users.noreply.github.com>
2024-06-03 09:49:32 -0400

    Add scan_angle vs lat_lon comparisons

--- goes2go/tools.py
+++ goes2go/tools.py
@@ -190,3 +190,111 @@ def glm_crs(G, reference_variable="flash_lat"):
     dat = G.metpy.parse_cf("flash_lat")
     crs = dat.metpy.cartopy_crs
     return crs
+
+
+def scan_angles_to_lat_lon(x, y, goes_imager_projection, decimal_degrees=True):
+    """
+    Convert ABI scan angle coordinates to geodetic latitude and longitude.
+
+    Scan angles are projected onto the GRS80 elliopsoid.
+    https://www.goes-r.gov/users/docs/PUG-L1b-vol3.pdf (section 5.1.2.8.1)
+
+    Parameters
+    ----------
+    x : float
+        A scan angle coordinate in the x direction.
+    y : float
+        A scan angle coordinate in the y direction.
+    goes_imager_projection : xarray.Dataset
+        An xarray.Dataset with the projection information in it's attributes.
+    decimal_degrees : bool
+        A boolean designating if latitude and longitude should be returned in decimal degrees (returns decimal radians if false).
+
+    Returns
+    -------
+    Two objects are returned
+        1. latitude on the GSR80 ellipsoid
+        2. longitude on the GSR80 ellipsoid
+    """
+    r_pol = goes_imager_projection.semi_minor_axis
+    r_eq = goes_imager_projection.semi_major_axis
+    H = goes_imager_projection.perspective_point_height + r_eq
+    lambda_0 = np.radians(
+        goes_imager_projection.longitude_of_projection_origin
+    )  # Always in degrees from the GOES data
+
+    a = np.sin(x) ** 2 + np.cos(x) ** 2 * (
+        np.cos(y) ** 2 + r_eq**2 / r_pol**2 * np.sin(y) ** 2
+    )
+    b = -2 * H * np.cos(x) * np.cos(y)
+    c = H**2 - r_eq**2
+
+    r_s = (-b - np.sqrt(b**2 - 4 * a * c)) / (
+        2 * a
+    )  # Distance from the satellite to point P
+
+    s_x = r_s * np.cos(x) * np.cos(y)
+    s_y = -r_s * np.sin(x)
+    s_z = r_s * np.cos(x) * np.sin(y)
+
+    latitude = np.arctan(
+        (r_eq**2 / r_pol**2) * (s_z / np.sqrt((H - s_x) ** 2 + s_y**2))
+    )
+    longitude = lambda_0 - np.arctan(s_y / (H - s_x))
+
+    if decimal_degrees:
+        latitude = np.degrees(latitude)
+        longitude = np.degrees(longitude)
+
+    return latitude, longitude
+
+
+def lat_lon_to_scan_angles(latitude, longitude, goes_imager_projection, decimal_degrees=True):
+    """
+    Convert geodetic latitude and longitude to ABI scan angle coordinates.
+
+    Latitude and Longitude inputs are assumed to be located on the GRS80 elliopsoid.
+    https://www.goes-r.gov/users/docs/PUG-L1b-vol3.pdf (section 5.1.2.8.1)
+
+    Parameters
+    ----------
+    latitude : float
+        Latitude on the GSR80 ellipsoid.
+    longitude : float
+        Longitude on the GSR80 ellipsoid.
+    goes_imager_projection : xarray.Dataset
+        An xarray.Dataset with the projection information in it's attributes.
+    decimal_degrees : bool
+        A boolean designating if latitude and longitude inputs are in decimal degrees (returns decimal radians if false).
+
+    Returns
+    -------
+    Two objects are returned
+        1. A scan angle coordinate in the x direction measured in radians
+        2. A scan angle coordinate in the y direction measured in radians
+    """
+    if decimal_degrees:
+        latitude = np.radians(latitude)
+        longitude = np.radians(longitude)
+    
+    r_pol = goes_imager_projection.semi_minor_axis
+    r_eq = goes_imager_projection.semi_major_axis
+    H = goes_imager_projection.perspective_point_height + r_eq
+    e = 0.0818191910435  # eccentricity of GRD 1980 ellipsoid
+    lambda_0 = np.radians(goes_imager_projection.longitude_of_projection_origin) # Always in degrees from the GOES data
+
+    phi_c = np.arctan((r_pol**2/r_eq**2)*np.tan(latitude))  # Geocentric latitude
+    r_c = r_pol/(np.sqrt(1-(e**2*np.cos(phi_c)**2)))  # Geocentric distance to the point on the ellipsoid
+
+    s_x = H - r_c*np.cos(phi_c)*np.cos(longitude-lambda_0)
+    s_y = -r_c*np.cos(phi_c)*np.sin(longitude-lambda_0)
+    s_z = r_c*np.sin(phi_c)
+
+    # Confirm location is visible from the satellite
+    if (H*(H-s_x) < (s_y**2 + (r_eq**2/r_pol**2)*s_z**2)).any():
+        raise ValueError("One or more points not visible by satellite")
+    
+    y = np.arctan(s_z/s_x)
+    x = np.arcsin(-s_y / (np.sqrt(s_x**2+s_y**2+s_z**2)))
+
+    return x, y
